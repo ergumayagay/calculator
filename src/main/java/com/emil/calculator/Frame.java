915e08fe3441c86f3ef3b90fcddf9f2459978af2
Nigel AOYANG <nigel.aoyang@tap-group.com.cn>
2018-10-05 15:28:41 +0800

    Changed UI to look like Calculator

---
+++ src/main/java/com/emil/calculator/Frame.java
@@ -0,0 +1,234 @@
+package com.emil.calculator;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import calculator.wsdl.AddResponse;
+
+@SpringBootApplication
+public class Frame extends JFrame {
+	private static final long serialVersionUID = 1907887853617040543L;
+
+	private JTextField input;
+	
+	@Autowired 
+	private CalculatorClient client;
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Frame.class).headless(false).run(args);
+		Frame frame = ctx.getBean(Frame.class);
+		frame.run();
+	}
+	
+	private void run() {
+		
+		setResizable(false);
+		setTitle("Calculator");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		JPanel contentPane = new JPanel();
+				
+		GridBagLayout grid = new GridBagLayout();
+		grid.columnWeights =  new double[] {};
+		grid.columnWidths = new int[] {100,100,100,100};
+		grid.rowWeights = new double[] {};
+		grid.rowHeights = new int[] {50,100,100,100,100};
+		
+		contentPane.setLayout(grid);
+		setContentPane(contentPane);
+
+		Font btnFont = new Font("Arial", 0, 20);
+		
+		JButton btn1 = new JButton("1");
+		btn1.setFont(btnFont);
+		btn1.addActionListener(new BtnAction());
+		JButton btn2 = new JButton("2");
+		btn2.setFont(btnFont);
+		btn2.addActionListener(new BtnAction());
+		JButton btn3 = new JButton("3");
+		btn3.setFont(btnFont);
+		btn3.addActionListener(new BtnAction());
+		JButton btn4 = new JButton("4");
+		btn4.setFont(btnFont);
+		btn4.addActionListener(new BtnAction());
+		JButton btn5 = new JButton("5");
+		btn5.setFont(btnFont);
+		btn5.addActionListener(new BtnAction());
+		JButton btn6 = new JButton("6");
+		btn6.setFont(btnFont);
+		btn6.addActionListener(new BtnAction());
+		JButton btn7 = new JButton("7");
+		btn7.setFont(btnFont);
+		btn7.addActionListener(new BtnAction());
+		JButton btn8 = new JButton("8");
+		btn8.setFont(btnFont);
+		btn8.addActionListener(new BtnAction());
+		JButton btn9 = new JButton("9");
+		btn9.setFont(btnFont);
+		btn9.addActionListener(new BtnAction());
+		JButton btn0 = new JButton("0");
+		btn0.setFont(btnFont);
+		btn0.addActionListener(new BtnAction());
+		
+		JButton btnAdd = new JButton("+");
+		btnAdd.setFont(btnFont);
+		btnAdd.addActionListener(new BtnAction());
+		JButton btnSub = new JButton("-");
+		btnSub.setFont(btnFont);
+		btnSub.addActionListener(new BtnAction());
+		JButton btnMul = new JButton("*");
+		btnMul.setFont(btnFont);
+		btnMul.addActionListener(new BtnAction());
+		JButton btnDiv = new JButton("/");
+		btnDiv.setFont(btnFont);
+		btnDiv.addActionListener(new BtnAction());
+		JButton btnDot = new JButton(".");
+		btnDot.setFont(btnFont);
+		btnDot.addActionListener(new BtnAction());
+		JButton btnEq  = new JButton("=");
+		btnEq.setFont(btnFont);
+		btnEq.addActionListener(e -> {
+			
+			
+			
+			
+			
+			
+		});
+
+		input = new JTextField();
+		input.setPreferredSize(new Dimension(300, 50));
+		input.setFont(new Font("Arial", 1, 20));
+		
+		GridBagConstraints gridCon = new GridBagConstraints();
+		
+		gridCon.fill = GridBagConstraints.BOTH;
+		
+		//First row
+		gridCon.gridx = 0;
+		gridCon.gridy = 1;
+		contentPane.add(btn1, gridCon);
+		
+		gridCon.gridx = 1;
+		gridCon.gridy = 1;
+		contentPane.add(btn2, gridCon);
+		
+		gridCon.gridx = 2;
+		gridCon.gridy = 1;
+		contentPane.add(btn3, gridCon);
+		
+		gridCon.gridx = 3;
+		gridCon.gridy = 1;
+		contentPane.add(btnDiv, gridCon);
+		
+		//Second Row
+		gridCon.gridx = 0;
+		gridCon.gridy = 2;
+		contentPane.add(btn4, gridCon);
+		
+		gridCon.gridx = 1;
+		gridCon.gridy = 2;
+		contentPane.add(btn5, gridCon);
+		
+		gridCon.gridx = 2;
+		gridCon.gridy = 2;
+		contentPane.add(btn6, gridCon);
+		
+		gridCon.gridx = 3;
+		gridCon.gridy = 2;
+		contentPane.add(btnMul, gridCon);
+		
+		//Third Row
+		gridCon.gridx = 0;
+		gridCon.gridy = 3;
+		contentPane.add(btn7, gridCon);
+		
+		gridCon.gridx = 1;
+		gridCon.gridy = 3;
+		contentPane.add(btn8, gridCon);
+		
+		gridCon.gridx = 2;
+		gridCon.gridy = 3;
+		contentPane.add(btn9, gridCon);
+		
+		gridCon.gridx = 3;
+		gridCon.gridy = 3;
+		contentPane.add(btnSub, gridCon);
+		
+		//Fourth Row
+		gridCon.gridx = 0;
+		gridCon.gridy = 4;
+		contentPane.add(btn0, gridCon);
+		
+		gridCon.gridx = 1;
+		gridCon.gridy = 4;
+		contentPane.add(btnDot, gridCon);
+		
+		gridCon.gridx = 2;
+		gridCon.gridy = 4;
+		contentPane.add(btnEq, gridCon);
+		
+		gridCon.gridx = 3;
+		gridCon.gridy = 4;
+		contentPane.add(btnAdd, gridCon);
+		
+		//Input
+		GridBagConstraints inputCon = new GridBagConstraints();
+		inputCon.gridx = 0;
+		inputCon.gridy = 0;
+		inputCon.gridwidth = 4;
+		contentPane.add(input, inputCon);
+		
+		pack();
+		
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screen = kit.getScreenSize();
+		
+		int posX = (int)((screen.getWidth() - getWidth()) / 2);
+		int posY = (int)((screen.getHeight() - getHeight()) / 2);
+		
+		setLocation(posX, posY);
+		
+		setVisible(true);
+	}
+	
+	private class BtnAction implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			JButton btn = (JButton)arg0.getSource();
+			input.setText(input.getText() + btn.getText());
+		}
+		
+		
+	}
+	
+	private class EqualsAction implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			String inputString = input.getText();
+			AddResponse sum = client.add(1, 2);
+			input.setText(Integer.toString(sum.getAddResult()));
+		}
+	}
+	
+
+}
